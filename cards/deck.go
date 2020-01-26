3622581be91632745c8a103f1da38e736d6e7cd6
zeuslawyer <zubin.pratap@gmail.com>
2020-01-26 19:10:56 +1100

    add shuffle()

--- cards/deck.go
+++ cards/deck.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"math/rand"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
 	"strings"
 )
 
 // create a new type called "Deck"
-// Deck is a slice of string types
+// Deck is a 'slice' of string types - i.e.  array of string types
 
 type Deck []string
 
@@ -22,7 +25,7 @@ func newDeck() Deck {
 			deck = append(deck, val+" of "+suit)
 		}
 	}
-
+	
 	deck = append(deck, "Joker 1", "Joker 2")
 
 	return deck
@@ -41,23 +44,57 @@ func (d Deck) print() {
 
 }
 
+/**
+returns multiple values
+*/
 func (d Deck) deal(numHand int) (Deck, Deck) {
-	hand := d[:numHand]
-	remainingCards := d[numHand:]
+	hand := d[:numHand]           // slice the slice and return
+	remainingCards := d[numHand:] // slice  the slice and return
 
 	return hand, remainingCards
 }
 
 func (d Deck) toString() string {
-	deckAsString := []string(d) // type convert(cast)
+	deckAsString := []string(d) // type convert(cast) deck into a string slice (array)
 
-	res := strings.Join(deckAsString, ",") // https://golang.org/pkg/io/ioutil/#WriteFile
+	deck := strings.Join(deckAsString, ",") // https://golang.org/pkg/io/ioutil/#WriteFile
 
-	return res
+	return deck
 }
 
 func (d Deck) saveToDisk(filename string) error {
-	content := []byte(d.toString()) // type cast
+	content := []byte(d.toString()) // type cast deck as a string, into []byte
 
 	return ioutil.WriteFile(filename, content, 0666) // https://golang.org/pkg/io/ioutil/#WriteFile
+}
+
+func loadDeckFromFile(filename string) Deck {
+
+	byteSlice, err := ioutil.ReadFile(filename) // https://golang.org/pkg/io/ioutil/#ReadFile
+
+	if err != nil {
+		log.Fatal("Error loading file:", err)
+		os.Exit(1) // https://golang.org/pkg/os/
+	} else {
+		log.Printf(" <<< File succesfully loaded >>>: ")
+	}
+
+	// fmt.Printf("File contents in a byte array/slice: %s", byteSlice)
+	// convert []bytes to string
+	sliceAsString := string(byteSlice)
+
+	// convert string to string[]
+	stringArr := strings.Split(sliceAsString, ",")
+	deck := Deck(stringArr)
+	return deck
+
+}
+
+
+func (d Deck) shuffle()  {
+	for i:= range d{
+		random:= rand.Intn(len(d)-1)
+		// swap
+		d[i], d[random] = d[random], d[i]
+	}
 }
--- cards/main.go
+++ cards/main.go
@@ -17,6 +17,13 @@ func main() {
 
 	// fmt.Println(len(cards))
 	res := cards.saveToDisk("testFileSave")
-	fmt.Println(res)
+	fmt.Println("response from saving file", res)
+
+	cardsFromFile := loadDeckFromFile("testFileSave")
+	fmt.Println("response from loading file:\n", cardsFromFile)
+
+
+	cardsFromFile.shuffle()
+	fmt.Println("Shuffled cards from file...\n", cardsFromFile)
 
 }
