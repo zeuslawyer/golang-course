e447fd913245321c5e4a69f2dc1dbab8252f89e7
zeuslawyer <zubin.pratap@gmail.com>
2020-02-12 21:10:02 +1100

    shortcut in declaring functions to accept pointers to mutate values

--- structs/main.go
+++ structs/main.go
@@ -19,20 +19,19 @@ func main() {
 		firstName:   "Jim",
 		lastname:    "Samsoo",
 		contactInfo: contactInfo{zipCode: 5455, email: "jimmysoo@jimmy.com"}}
-
-	jimPointer := &jim   
-	jimPointer.updateName("Barry")
+ 
+	jim.updateName("Barry")
 
 	jim.print()
 }
  
 // receiver gets a pointer so that it updates the actual values, otherwise it will pass a copy of jim and that wont work!
 // this is necessary when we want to mutate the value received in the receiver
-func (personPointer *person) updateName(newFirstName string) *person {
-	// (*personPointer).firstName = newFirstName
-	return personPointer
+func (personPointer *person) updateName(newFirstName string) person {
+	(*personPointer).firstName = newFirstName
+	return *personPointer // return val not pointer
 }
 
 func (p person) print() {
-	fmt.Printf("%+v\n", p.contactInfo) // prints out the key + value (entire struct/obj). just %v will give you only the values
+	fmt.Printf("%+v\n", p) // prints out the key + value (entire struct/obj). just %v will give you only the values
 }
